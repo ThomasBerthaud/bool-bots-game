412cf1dc8d968b534e9a7f47143d4d0025d831d5
Thomas Berthaud <thomas.berthaud@viseo.com>
2022-03-11 21:29:05 +0100

    fix: fix + improve build split

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import analyze from "rollup-plugin-analyzer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    pixi: ["pixi.js"],
+                },
+            },
+            plugins: [analyze({ summaryOnly: true })],
+        },
+    },
     plugins: [react()],
 });
